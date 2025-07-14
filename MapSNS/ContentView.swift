b856a6fa204252a3342b91dadc487a7bd8eb8292
obayashi koshin <bayamax@icloud.com>
2025-07-15 00:22:17 +0900

    Initial Commit

---
+++ MapSNS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MapSNS
+//
+//  Created by 大林恒心 on 2025/07/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MapSNS/MapSNSApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapSNSApp.swift
+//  MapSNS
+//
+//  Created by 大林恒心 on 2025/07/15.
+//
+
+import SwiftUI
+
+@main
+struct MapSNSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
